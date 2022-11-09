888b635920d6f1e1287cf7f644315221fdfa0b00
Xejb <vladiq-artamonenko@rambler.ru>
2022-11-09 14:15:12 +0300

    status ip

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def get_info_by_ip(ip='127.0.0.1'):
         print(response)
     except requests.exceptions.ConnectionError:
         print('[!] Please check your connection!')
+
+
+def main():
+    ip = input('Please enter a target IP: ')
+
+    get_info_by_ip(ip=ip)
+
+if __name__ == '__main__':
+    main()
