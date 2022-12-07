6c8fbbaafac5241533b06632fca49c4fa7f7059b
Xejb <vladiq-artamonenko@rambler.ru>
2022-12-07 18:13:48 +0300

    upd

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bros.views import BrosAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/broslist/', BrosAPIView.as_view())
 ]
