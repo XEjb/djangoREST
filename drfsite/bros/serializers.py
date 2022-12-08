108aafc5047c845c0eb7724b7dabb2ea17772a74
Xejb <vladiq-artamonenko@rambler.ru>
2022-12-08 14:42:16 +0300

    Serializer

--- drfsite/bros/serializers.py
+++ drfsite/bros/serializers.py
@@ -1,8 +1,36 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from .models import Bros
 
 
-class BrosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Bros
-        fields = ('title', 'cat_id')
+# class BrosModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class BrosSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+# def encode():
+#     model = BrosModel('Nik', 'Content: Nik')
+#     model_sr = BrosSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Nik","content":"Content: Nik"}')
+#     data = JSONParser().parse(stream)
+#     serializer = BrosSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
--- drfsite/bros/views.py
+++ drfsite/bros/views.py
@@ -9,17 +9,20 @@ from .serializers import BrosSerializer
 
 class BrosAPIView(APIView):
     def get(self, request):
-        lst = Bros.objects.all().values()
-        return Response({'posts': list(lst)})
+        w = Bros.objects.all()
+        return Response({'posts': BrosSerializer(w, many=True).data})
 
     def post(self, request):
+        serializer = BrosSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = Bros.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
 
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': BrosSerializer(post_new).data})
 
 # class BrosAPIView(generics.ListAPIView):
 #     queryset = Bros.objects.all()
