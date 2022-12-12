c36a3f0a90c3538a872605bfa2ecae009a10b4f5
Xejb <vladiq-artamonenko@rambler.ru>
2022-12-12 16:53:49 +0300

    Pagination

--- drfsite/bros/views.py
+++ drfsite/bros/views.py
@@ -2,6 +2,7 @@ from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,21 +13,27 @@ from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import BrosSerializer
 
 
+class BrosAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class BrosAPIList(generics.ListCreateAPIView):
     queryset = Bros.objects.all()
     serializer_class = BrosSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = BrosAPIListPagination
 
 
 class BrosAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Bros.objects.all()
     serializer_class = BrosSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
     # authentication_classes = (TokenAuthentication, )
 
 
 class BrosAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Bros.objects.all()
     serializer_class = BrosSerializer
-    permission_classes = (IsAdminOrReadOnly, )
-
+    permission_classes = (IsAdminOrReadOnly,)
